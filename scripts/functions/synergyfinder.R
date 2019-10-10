37c1a58b81768ff4ca5fc20f02901e8ee2655e17
Niklas <niklastimon@googlemail.com>
2019-10-10 18:29:56 +0200

    local push

--- scripts/functions/synergyfinder.R
+++ scripts/functions/synergyfinder.R
@@ -2,16 +2,24 @@ library(synergyfinder)
 
 tidy_synergy = function(df, me){ lapply(as.list(me), function(m, f = df){
   #hacky way of ignoring cases in which the synergy scores could not be calculated
-  return(tryCatch(f %>% 
-                    ReshapeData(., data.type = "viability") %>%
+  return(
+    #tryCatch(
+    f %>% 
+                    ReshapeData(., data.type = "viability",
+                                impute = TRUE, 
+                                noise = TRUE, 
+                                correction = "all") %>%
                     #PlotDoseResponse() %>%
-                    CalculateSynergy(method = m, correction = TRUE) %>%
+                    CalculateSynergy(method = m) %>% #, correction = TRUE
                     tibble(BlockID = .$drug.pairs %>% simplify() %>% .[4],
                            method = .$method,
                            score_average = .$scores %>% .[[1]] %>% .[-1,-1] %>% simplify() %>% mean(),
                            score_sd = .$scores %>% .[[1]] %>% .[-1,-1] %>% simplify() %>% sd()) %>%
-                    select(BlockID, method, score_average, score_sd) %>% distinct(), 
-                  error=function(e) NULL))}
+                    select(BlockID, method, score_average, score_sd) %>% distinct()#, 
+      #              error=function(e) 
+       #             NULL)
+  )
+  }
 ) %>% bind_rows()
   #PlotSynergy(type = "all")
 }
